577ebb805cb1d1b5130a959bfe62561aef60aa37
Ivan Dario Sanchez Fonseca <isanchez@intergrupo.com>
2017-10-16 08:31:02 -0500

    Agrego proyecto de prueba

---
+++ BooksApp.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksApp.Models;
+using BooksApp.ResilientHttp.ResilienceHttp;
+using BooksApp.ProxyClient;
+
+namespace BooksApp.Console
+{
+    class Program
+    {
+        private static BaseClient<Book> _bookContextWs;
+
+        static void Main(string[] args)
+        {
+            _bookContextWs = new BaseClient<Book>("Books", new ResilientHttpClientFactory().CreateResilientHttpClient());
+            var respuesta = Task.Factory.StartNew(async ()=> await _bookContextWs.GetAll()).Result.Result.ToList();
+        }
+    }
+}
